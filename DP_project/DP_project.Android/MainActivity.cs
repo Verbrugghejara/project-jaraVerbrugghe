1250c88a151d311be9d412aae3bbec8f2edcdec9
Jara <jara.verbrugghe@student.howest.be>
2021-12-03 19:06:05 +0100

    change frontend in CreateProject

--- DP_project/DP_project.Android/MainActivity.cs
+++ DP_project/DP_project.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace DP_project.Droid
 {
-    [Activity(Label = "DP_project", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "DP_project", Icon = "@mipmap/icon", Theme = "@style/MainTheme.Base2", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
